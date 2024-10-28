c6472b2ac39c1f4b12310a22d10468a3975d0efc
엄태준 <tj0306@eomtaejun-ui-MacBookPro.local>
2024-10-28 16:59:50 +0900

    read, create

--- letsStart/src/app.ts
+++ letsStart/src/app.ts
@@ -1,26 +1,78 @@
 import * as express from "express";
 import { Cat, CatType } from "./app.model";
+import { error } from "console";
 
 const app: express.Express = express();
 
+//* logging middleware
 app.use((req, res, next) => {
   console.log(req.rawHeaders[1]);
   console.log("this is middleware");
   next();
 });
 
-app.get("/", (req, res) => {
-  res.send({ cats: Cat });
+//* READ 고양이 데이터 전부 조회
+app.get("/cats", (req, res) => {
+  try {
+    const cats = Cat;
+    throw new Error("db connect error");
+    res.send({
+      success: true,
+      data: {
+        cats,
+      },
+    });
+  } catch (error) {
+    res.status(400).send({
+      success: false,
+      error: error.message,
+    });
+  }
 });
 
-app.get("/cats/blue", (req, res) => {
-  res.send({ blue: Cat[0] });
+//* READ 특정 고양이 데이터 조회
+app.get("/cats/:id", (req, res) => {
+  try {
+    const params = req.params;
+    console.log(req.params);
+    const cat = Cat.find((cat) => {
+      return cat.id === params.id;
+    });
+    res.status(200).send({
+      success: true,
+      data: {
+        cat,
+      },
+    });
+  } catch (error) {
+    res.status(400).send({
+      success: false,
+      error: error.message,
+    });
+  }
 });
 
-app.get("/cats/som", (req, res) => {
-  res.send({ som: Cat[1] });
+//* json middleware
+app.use(express.json());
+
+//* CREATE 새로운
+app.post("/cats", (req, res) => {
+  try {
+    const data = req.body;
+    Cat.push(data); // create
+    res.status(200).send({
+      success: true,
+      data: { data },
+    });
+  } catch (error) {
+    res.status(400).send({
+      success: false,
+      error: error.message,
+    });
+  }
 });
 
+//* 404 middleware
 app.use((req, res, next) => {
   res.send({ error: "404 not found error" });
 });
